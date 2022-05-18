42f0db916974ee898e73de633ead3bb5b51147d9
Tatiana Simmer <tatkagore@gmail.com>
2022-05-18 14:22:09 +0200

    Initial Commit

---
+++ Shared/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Shared
+//
+//  Created by Tatiana Simmer on 18/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
